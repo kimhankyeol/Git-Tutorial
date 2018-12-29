6033258b6c47afd89f00774c21983a848779c11e
kimhankyeol <kimhk1030@naver.com>
2018-12-29 17:05:18 +0900

    Add TestGit [Add & update]

--- TestGit.java
+++ TestGit.java
@@ -1,6 +1,9 @@
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("김한결 테스트중");
+        System.out.println("kim test");
+
+	System.out.println("kim commit update");
+	
     }
 
 }
